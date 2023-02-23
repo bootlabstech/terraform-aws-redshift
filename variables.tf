b8eee1874f1ec3e5230b8f5f69f72ca105d40804
S-Divyadharshini <sdivya-cont@mahindra.com>
2023-02-23 13:27:24 +0530

    fix

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "cluster_identifier" {
   description = "The Cluster Identifier. Must be a lower case string"
   type        = string
+  default     = ""
 }
 variable "database_name" {
   description = "The name of the first database to be created when the cluster is created. If you do not provide a name, Amazon Redshift will create a default database called `dev`"
   type        = string
-  default     = null
 }
 variable "master_password" {
   description = "Password for the master DB user. (Required unless a `snapshot_identifier` is provided). Must contain at least 8 chars, one uppercase letter, one lowercase letter, and one number"
@@ -21,16 +21,16 @@ variable "master_username" {
 variable "node_type" {
   description = "The node type to be provisioned for the cluster"
   type        = string
+  default     = ""
 }
 variable "publicly_accessible" {
   description = "If true, the cluster can be accessed from a public network"
   type        = bool
-  default     = false
 }
 variable "automated_snapshot_retention_period" {
   description = "The number of days that automated snapshots are retained. If the value is 0, automated snapshots are disabled. Even if automated snapshots are disabled, you can still create manual snapshots when you want with create-cluster-snapshot. Default is 1"
   type        = number
-  default     = 7
+  default     = null
 }
 variable "logging_enabled" {
   type        = bool
@@ -52,10 +52,5 @@ variable "logging_s3_key_prefix" {
 variable "enhanced_vpc_routing" {
   type        = bool
   description = "If true , enhanced VPC routing is enabled"
-  default     = true
-}
-variable "allow_version_upgrade" {
-  type        = bool
-  default     = true
-  description = "Whether or not to enable major version upgrades which are applied during the maintenance window to the Amazon Redshift engine that is running on the cluster"
+  default     = false
 }
